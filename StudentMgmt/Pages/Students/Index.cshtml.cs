721ff758f88b5529793566485ec9c74eee659e30
Amrit <stanamrit@gmail.com>
2022-12-23 19:48:49 +0545

    first committ

---
+++ StudentMgmt/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentMgmt.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
